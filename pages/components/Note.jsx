8673258e31445c8d6b631475f33555d6ff77369e
vinayudaya <vinayudaya0509@gmail.com>
2024-02-11 20:31:06 +0530

    css

--- pages/components/Note.jsx
+++ pages/components/Note.jsx
@@ -9,7 +9,7 @@ function Note(props) {
     <div className="note">
       <h1>{props.title}</h1>
       <p>{props.content}</p>
-      <button onClick={handleClick}>DELETE</button>
+      <button onClick={handleClick}>Delete</button>
     </div>
   );
 }
